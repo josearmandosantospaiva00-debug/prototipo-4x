f08c672c299dc40fb0eb7976921e200873f71330
Jose Armando <seu_email@exemplo.com>
2026-03-10 14:48:37 -0300

    Adicionado Fog of War e sistema de coleta de cristais

--- mapa.gd
+++ mapa.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+var cristais_coletados = 0
+@onready var label_cristais = $CanvasLayer/LabelCristais # Verifique se o nome está igual ao do nó
 var raio = 40.0
 var colunas = 30
 var linhas = 20
@@ -51,16 +52,31 @@ func criar_hexagono(col, row):
 	x += 100
 	y += 100
 	poly.position = Vector2(x, y)
+	poly.set_script(load("res://polygon_2d.gd"))
 	poly.polygon = gerar_pontos()
 	
 	var chance = randf()
 	var custo = 1
+	var cor_escolhida
 	if chance < 0.30:
-		poly.color = Color(0.1, 0.4, 0.1) # Floresta
+		cor_escolhida = Color(0.1, 0.4, 0.1) # Floresta
 		custo = 2
 	else:
-		poly.color = Color(0.4, 0.7, 0.4) # Planície
+		cor_escolhida = Color(0.4, 0.7, 0.4) # Planície
 	
+	# Em vez de mudar a cor direto, chamamos a função que esconde
+	poly.configurar_cores(cor_escolhida)
+	# Chance de ter um cristal (15%)
+	if randf() < 0.15:
+		poly.set_meta("tem_cristal", true)
+		# Vamos colocar um pontinho visual para sabermos que tem cristal
+		var cristal_visual = ColorRect.new()
+		cristal_visual.size = Vector2(10, 10)
+		cristal_visual.position = Vector2(-5, -5) # Centraliza no hexágono
+		cristal_visual.color = Color.CYAN # Azul brilhante
+		poly.add_child(cristal_visual)
+	else:
+		poly.set_meta("tem_cristal", false)
 	poly.set_meta("custo", custo)
 	add_child(poly)
 	lista_hexagonos.append(poly)
@@ -109,6 +125,7 @@ func _input(event):
 					
 					var tween = create_tween()
 					tween.tween_property(unidade, "position", hex_clicado.position, 0.3)
+					tween.finished.connect(apos_movimento)
 					tween.set_trans(Tween.TRANS_SINE)
 					tween.set_ease(Tween.EASE_OUT)
 					# ATUALIZA A UI APÓS MOVER
@@ -120,3 +137,30 @@ func _on_button_pressed() -> void:
 	pontos_movimento_atuais = pontos_movimento_maximos
 	atualizar_ui()
 	print("Botão clicado! Turno resetado.")
+func atualizar_visao():
+	var raio_visao = raio * 3.5 # Define o quão longe o robô enxerga
+	
+	for hex in lista_hexagonos:
+		var distancia = unidade.position.distance_to(hex.position)
+		if distancia <= raio_visao:
+			hex.revelar()
+func checar_coleta():
+	for hex in lista_hexagonos:
+		# Se o robô está na mesma posição do hexágono e tem cristal
+		if unidade.position == hex.position and hex.get_meta("tem_cristal"):
+			cristais_coletados += 1
+			hex.set_meta("tem_cristal", false) # Remove o cristal do dado
+			
+			# Remove o visual do cristal (o primeiro filho do hexágono)
+			for child in hex.get_children():
+				if child is ColorRect:
+					child.queue_free()
+			
+			atualizar_ui_cristais()
+			print("Cristal coletado! Total: ", cristais_coletados)
+
+func atualizar_ui_cristais():
+	label_cristais.text = "Cristais: " + str(cristais_coletados)
+func apos_movimento():
+	atualizar_visao()
+	checar_coleta()
